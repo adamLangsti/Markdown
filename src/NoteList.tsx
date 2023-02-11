716e2766fa25a244f709ddfd0d0bc71142167efb
Adam Lang <adam.lang-1@hotmail.com>
2023-02-11 14:12:04 +0200

    feat: create tag in new notes

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,16 @@
-import { Button, Col, Row, Stack } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import ReactSelect from 'react-select';
+import { Tag } from './App';
 
-export function NoteList() {
+type NoteListProps = {
+    availableTags: Tag[];
+};
+
+export function NoteList({ availableTags }: NoteListProps) {
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
     return (
         <>
             <Row>
@@ -14,11 +23,53 @@ export function NoteList() {
                             <Button variant='primary'>Create</Button>
                         </Link>
                         <Link to='/new'>
-                            <Button variant='outline-secondary'>Edit Tags</Button>
+                            <Button variant='outline-secondary'>
+                                Edit Tags
+                            </Button>
                         </Link>
                     </Stack>
                 </Col>
             </Row>
+            <Form>
+                <Row className='mb-4'>
+                    <Col>
+                        <Form.Group controlId='title'>
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type='text' />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId='tags'>
+                            <Form.Label>Tags</Form.Label>
+                            <ReactSelect
+                                value={selectedTags.map((tag) => {
+                                    return {
+                                        label: tag.label,
+                                        value: tag.id,
+                                    };
+                                })}
+                                options={availableTags.map((tag) => {
+                                    return {
+                                        label: tag.label,
+                                        value: tag.id,
+                                    };
+                                })}
+                                onChange={(tags) => {
+                                    setSelectedTags(
+                                        tags.map((tag) => {
+                                            return {
+                                                label: tag.label,
+                                                id: tag.value,
+                                            };
+                                        })
+                                    );
+                                }}
+                                isMulti
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Form>
         </>
     );
 }
