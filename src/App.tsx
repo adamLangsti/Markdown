36fe1d12130a112917f262dcbcd3b75800ae5729
Adam Lang <adam.lang-1@hotmail.com>
2023-02-11 15:37:24 +0200

    new component NoteLayout

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { NewNote } from './NewNote';
 import { useLocalStorage } from './useLocalStorage';
 import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './NoteList';
+import { NoteLayout } from './NoteLayout';
 
 export type Note = {
     id: string;
@@ -61,7 +62,12 @@ function App() {
     return (
         <Container className='my-4'>
             <Routes>
-                <Route path='/' element={<NoteList availableTags={tags} notes={notesWithTags} />} />
+                <Route
+                    path='/'
+                    element={
+                        <NoteList availableTags={tags} notes={notesWithTags} />
+                    }
+                />
                 <Route
                     path='/new'
                     element={
@@ -72,7 +78,9 @@ function App() {
                         />
                     }
                 />
-                <Route path='/:id'>
+                <Route
+                    path='/:id'
+                    element={<NoteLayout notes={notesWithTags} />}>
                     <Route index element={<h1>Show</h1>} />
                     <Route path='edit' element={<h1>Edit</h1>} />
                 </Route>
