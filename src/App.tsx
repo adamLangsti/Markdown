21c90dbe9804c6797791a5ebacff3cf7c344d579
Adam Lang <adam.lang-1@hotmail.com>
2023-02-11 16:19:46 +0200

    feat: update function to edit existing notes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './NoteList';
 import { NoteLayout } from './NoteLayout';
 import { Note } from './Note';
+import { EditNote } from './EditNote';
 
 export type Note = {
     id: string;
@@ -56,6 +57,22 @@ function App() {
         });
     }
 
+    function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+        setNotes((prevNotes) => {
+            return prevNotes.map((note) => {
+                if (note.id === id) {
+                    return {
+                        ...note,
+                        ...data,
+                        tagIds: tags.map((tag) => tag.id),
+                    };
+                } else {
+                    return note;
+                }
+            });
+        });
+    }
+
     function addTag(tag: Tag) {
         setTags((prev) => [...prev, tag]);
     }
@@ -83,7 +100,16 @@ function App() {
                     path='/:id'
                     element={<NoteLayout notes={notesWithTags} />}>
                     <Route index element={<Note />} />
-                    <Route path='edit' element={<h1>Edit</h1>} />
+                    <Route
+                        path='edit'
+                        element={
+                            <EditNote
+                                onSubmit={onUpdateNote}
+                                onAddTag={addTag}
+                                availableTags={tags}
+                            />
+                        }
+                    />
                 </Route>
                 <Route path='*' element={<Navigate to='/' />} />
             </Routes>
