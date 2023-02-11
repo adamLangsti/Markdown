6dee3cf6652a0db0ad8fbf894e6d64ba5cd5200a
Adam Lang <adam.lang-1@hotmail.com>
2023-02-11 15:06:15 +0200

    feat: check if saved notes has information, then display them with badge

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,12 +1,19 @@
 import { useMemo, useState } from 'react';
-import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
+import { Badge, Button, Card, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import ReactSelect from 'react-select';
-import { Note, Tag } from './App';
+import { Tag } from './App';
+import styles from './NoteList.module.css';
+
+type SimplifiedNote = {
+    tags: Tag[];
+    title: string;
+    id: string;
+};
 
 type NoteListProps = {
     availableTags: Tag[];
-    notes: Note[];
+    notes: SimplifiedNote[];
 };
 
 export function NoteList({ availableTags, notes }: NoteListProps) {
@@ -92,10 +99,43 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
             <Row xs={1} sm={2} lg={3} xl={4} className='g-3'>
                 {filteredNotes.map((note) => (
                     <Col id={note.id}>
-                        <NoteCard />
+                        <NoteCard
+                            id={note.id}
+                            title={note.title}
+                            tags={note.tags}
+                        />
                     </Col>
                 ))}
             </Row>
         </>
+    );
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+    return (
+        <Card
+            as={Link}
+            to={`/${id}`}
+            className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+            <Card.Body>
+                <Stack
+                    gap={2}
+                    className='align-items-center justify-content-center h-100'>
+                    <span className='fs-5'>{title}</span>
+                    {tags.length > 0 && (
+                        <Stack
+                            gap={1}
+                            direction='horizontal'
+                            className='justify-content-center flew-wrap'>
+                            {tags.map((tag) => (
+                                <Badge className='text-truncate' key={tag.id}>
+                                    {tag.label}
+                                </Badge>
+                            ))}
+                        </Stack>
+                    )}
+                </Stack>
+            </Card.Body>
+        </Card>
     );
 }
