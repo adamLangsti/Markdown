ec2415ec72b8ee80778768686bd65eeb98fcb684
Adam Lang <adam.lang-1@hotmail.com>
2023-02-11 13:42:27 +0200

    feat: new homescreen component to create or edit tags

---
+++ src/NoteList.tsx
@@ -0,0 +1,24 @@
+import { Button, Col, Row, Stack } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export function NoteList() {
+    return (
+        <>
+            <Row>
+                <Col>
+                    <h1>Notes</h1>
+                </Col>
+                <Col xs='auto'>
+                    <Stack gap={2} direction='horizontal'>
+                        <Link to='/new'>
+                            <Button variant='primary'>Create</Button>
+                        </Link>
+                        <Link to='/new'>
+                            <Button variant='outline-secondary'>Edit Tags</Button>
+                        </Link>
+                    </Stack>
+                </Col>
+            </Row>
+        </>
+    );
+}
